d25dd58df7319ccd31f30204e8ce3c16ff2b7225
Antonio Zanatta <antoniogsztt@gmail.com>
2024-06-10 17:36:27 +0000

    aula

---
+++ my-app-react/src/App.js
@@ -0,0 +1,10 @@
+
+export default function App(){
+  const name = 'Antonio Zanatta'
+  return (
+    <div>
+      <h1>................................</h1>
+      <h2>;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;</h2>
+    </div>
+  )
+}
